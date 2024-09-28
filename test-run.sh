c5b568684e2d0cafa16d44b247365ee50e0c7f93
Sriram Srinivasan <sriram@us.ibm.com>
2024-09-28 14:20:18 -0700

    only need to mount for the "dev" case

--- test-run.sh
+++ test-run.sh
@@ -6,4 +6,4 @@ image="py-api-sample:v1"
 cname=py-api
 
 docker rm -f ${cname}
-docker run --name ${cname} -d --user 1001 -p 20080:20080 -v ${PWD}/src:/mnt/src ${image}
+docker run --name ${cname} -d --user 1001 -p 20080:20080 ${image}
